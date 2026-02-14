7c882059e5d025e48f8f9bc6a8418c20e24cb72e
luongphatlp <luongphat1123@gmail.com>
2026-02-14 15:18:32 +0700

    13-02-2026

--- src/DAO/ThongKeSanPhamDAO.java
+++ src/DAO/ThongKeSanPhamDAO.java
@@ -10,7 +10,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 /**
  *
  * @author Latitude E7470
