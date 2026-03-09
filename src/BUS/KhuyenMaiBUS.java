da609a3f5bc223216fb74ce8d988137aca0b5f29
DPEldich <nhanphan598@gmail.com>
2026-03-09 20:32:21 +0700

    Fix merge conflicts in KhuyenMaiBUS and DAO

--- src/BUS/KhuyenMaiBUS.java
+++ src/BUS/KhuyenMaiBUS.java
@@ -1,48 +1,57 @@
-
 package BUS;
 
 import DAO.KhuyenMaiDAO;
 import DTO.KhuyenMaiDTO;
 import java.util.ArrayList;
 
-
 public class KhuyenMaiBUS {
+
     private ArrayList<KhuyenMaiDTO> ds;
-    KhuyenMaiDAO dao=new KhuyenMaiDAO();
-    public KhuyenMaiBUS(){
-        if(ds==null) ds=new ArrayList<>();
+    KhuyenMaiDAO dao = new KhuyenMaiDAO();
+
+    public KhuyenMaiBUS() {
+        if (ds == null) {
+            ds = new ArrayList<>();
+        }
     }
-    public ArrayList<KhuyenMaiDTO> getDS(){return ds;}
-    
-    public ArrayList<KhuyenMaiDTO> docDS(){
-        ds=dao.selectAll();
+
+    public ArrayList<KhuyenMaiDTO> getDS() {
         return ds;
     }
-    public void add(KhuyenMaiDTO km){
+
+    public ArrayList<KhuyenMaiDTO> docDS() {
+        ds = dao.selectAll();
+        return ds;
+    }
+
+    public void add(KhuyenMaiDTO km) {
         dao.insert(km);
     }
-    public boolean kiemTraMaKhuyenMaiTonTai(String ma){
-        for(KhuyenMaiDTO km:ds){
-            if(km.getMa().equals(ma)){
+
+    public boolean kiemTraMaKhuyenMaiTonTai(String ma) {
+        for (KhuyenMaiDTO km : ds) {
+            if (km.getMa().equals(ma)) {
                 return true;
             }
         }
         return false;
     }
-    public void update(KhuyenMaiDTO km){
+
+    public void update(KhuyenMaiDTO km) {
         dao.update(km);
     }
-    public ArrayList<String> getDSMaKMHoatDong() {
-    ArrayList<String> dsMa = new ArrayList<>();
-    ArrayList<KhuyenMaiDTO> dsAll = kmDAO.getList(); // Giả sử bạn đã có hàm getList() lấy tất cả
-    
-    for (KhuyenMaiDTO km : dsAll) {
-        // Kiểm tra TinhTrang = 1 (Hoạt động)
-        // Bạn có thể thêm điều kiện kiểm tra ngày hệ thống nằm trong khoảng NgayBatDau và NgayKetThuc
-        if (km.getTinhTrang() == 1) { 
-            dsMa.add(km.getMa());
-        }
-    }
-    return dsMa;
-}
+
+//    public ArrayList<String> getDSMaKMHoatDong() {
+////    ArrayList<String> dsMa = new ArrayList<>();
+////    ArrayList<KhuyenMaiDTO> dsAll = kmDAO.getList(); // Giả sử bạn đã có hàm getList() lấy tất cả
+////    
+////    for (KhuyenMaiDTO km : dsAll) {
+////        // Kiểm tra TinhTrang = 1 (Hoạt động)
+////        // Bạn có thể thêm điều kiện kiểm tra ngày hệ thống nằm trong khoảng NgayBatDau và NgayKetThuc
+////        if (km.getTinhTrang() == 1) { 
+////            dsMa.add(km.getMa());
+////        }
+////    }
+////    return dsMa;
+//    }
 }
