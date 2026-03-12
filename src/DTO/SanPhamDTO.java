e930fbb6995262a3e7d7adda2988dc732943dd7c
Vo Thanh Tai <taivothanh@gmail.com>
2026-03-12 23:08:05 +0700

    ok

--- src/DTO/SanPhamDTO.java
+++ src/DTO/SanPhamDTO.java
@@ -1,48 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DTO;
 
-/**
- *
- * @author Latitude E7470
- */
 public class SanPhamDTO {
-    private String masp,ten,dongia,donvitinh,mahang;
-    private int soluong;
+    private String masp, tenSP, donvitinh, mahang;
+    private int soluong, dongia;
+
     public SanPhamDTO(){}
-    public SanPhamDTO(String masp,String ten,int soluong,String dongia,String donvitinh,String mahang){
-        this.masp=masp;
-        this.ten=ten;
-        this.soluong=soluong;
-        this.dongia=dongia;
-        this.donvitinh=donvitinh;
-        this.mahang=mahang;
+    public SanPhamDTO(String masp, String tenSP, int soluong, int dongia, String donvitinh, String mahang){
+        this.masp = masp;
+        this.tenSP = tenSP;
+        this.soluong = soluong;
+        this.dongia = dongia;
+        this.donvitinh = donvitinh;
+        this.mahang = mahang;
     }
     public SanPhamDTO(SanPhamDTO sp){
-        this.masp=sp.masp;
-        this.ten=sp.ten;
-        this.soluong=sp.soluong;
-        this.dongia=sp.dongia;
-        this.donvitinh=sp.donvitinh;
-        this.mahang=sp.mahang;
+        this.masp = sp.masp;
+        this.tenSP = sp.tenSP;
+        this.soluong = sp.soluong;
+        this.dongia = sp.dongia;
+        this.donvitinh = sp.donvitinh;
+        this.mahang = sp.mahang;
     }
-    public String getMaSP(){return masp;}
-    public void setMaSP(String masp){this.masp=masp;}
-    
-    public String getTen(){return ten;}
-    public void setTen(String ten){this.ten=ten;}
-    
-    public int getSoLuong(){return soluong;}
-    public void setSoLuong(int sl){this.soluong=sl;}
-    
-    public String getDonGia(){return dongia;}
-    public void setDonGia(String dongia){this.dongia=dongia;}
-    
-    public String getDonViTinh(){return donvitinh;}
-    public void setDonViTinh(String donvitinh){this.donvitinh=donvitinh;}
-    
-    public String getMaHang(){return mahang;}
-    public void setMaHang(String mahang){this.mahang=mahang;}
+    public String getMaSP(){ return masp; }
+    public void setMaSP(String masp){ this.masp = masp; }
+    public String getTenSP(){ return tenSP; } 
+    public void setTenSP(String tenSP){ this.tenSP = tenSP; }
+    public int getSoLuong(){ return soluong; }
+    public void setSoLuong(int sl){ this.soluong = sl; }
+    public int getDonGia(){ return dongia; }
+    public void setDonGia(int dongia){ this.dongia = dongia; }  
+    public String getDonViTinh(){ return donvitinh; }
+    public void setDonViTinh(String donvitinh){ this.donvitinh = donvitinh; }  
+    public String getMaHang(){ return mahang; }
+    public void setMaHang(String mahang){ this.mahang = mahang; }
 }
