147a9401d690f57fa46d0d40583494054731bcc3
CholinE7 <nguyentrannamthinh0711@gmail.com>
2026-03-21 22:41:15 +0700

    2240 21/03

---
+++ src/DATABASE/Connect.java
@@ -0,0 +1,32 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connect {
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/chdt", "root", "");
+
+        } catch (Exception ex) {
+             ex.printStackTrace();
+
+        }
+        return con;
+    }
+
+    public static void closeConnection(Connection c) {
+        try {
+            if (c != null) {
+                c.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
