fd7257af2ac8ddef19d20c526e047f98e583c18d
Vo Thanh Tai <taivothanh@gmail.com>
2026-03-21 23:39:38 +0700

    update SanPhamDTO

--- src/DTO/SanPhamDTO.java
+++ src/DTO/SanPhamDTO.java
@@ -1,40 +1,38 @@
 package DTO;
 
 public class SanPhamDTO {
-    private String masp, tenSP, donvitinh, mahang;
+    private String masp, tenSP, boNho;
     private int soluong, dongia;
     private ChiTietSanPhamDTO chiTiet;
-    private String boNho; 
 
     public SanPhamDTO() {
         this.chiTiet = new ChiTietSanPhamDTO();
     }
     
-    public SanPhamDTO(String masp, String tenSP, int soluong, int dongia, String donvitinh, String mahang) {
+    public SanPhamDTO(String masp, String tenSP, int soluong, int dongia, String boNho) {
         this.masp = masp;
         this.tenSP = tenSP;
         this.soluong = soluong;
         this.dongia = dongia;
-        this.donvitinh = donvitinh;
-        this.mahang = mahang;
+        this.boNho = boNho;
         this.chiTiet = new ChiTietSanPhamDTO();
     }
 
     public String getMaSP() { return masp; }
     public void setMaSP(String masp) { this.masp = masp; this.chiTiet.setMaSP(masp); }
+    
     public String getTenSP() { return tenSP; }
     public void setTenSP(String tenSP) { this.tenSP = tenSP; }
+    
     public int getSoLuong() { return soluong; }
     public void setSoLuong(int sl) { this.soluong = sl; }
+    
     public int getDonGia() { return dongia; }
     public void setDonGia(int dongia) { this.dongia = dongia; }
-    public String getDonViTinh() { return donvitinh; }
-    public void setDonViTinh(String donvitinh) { this.donvitinh = donvitinh; }
-    public String getMaHang() { return mahang; }
-    public void setMaHang(String mahang) { this.mahang = mahang; }
+    
+    public String getBoNho() { return boNho; }
+    public void setBoNho(String boNho) { this.boNho = boNho; }
 
     public ChiTietSanPhamDTO getChiTiet() { return chiTiet; }
     public void setChiTiet(ChiTietSanPhamDTO chiTiet) { this.chiTiet = chiTiet; }
-    public String getBoNho() { return boNho; }
-    public void setBoNho(String boNho) { this.boNho = boNho; }
 }
